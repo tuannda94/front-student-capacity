69a5914b39f892a69019c5820311c561dfbf85d9
vanquang202 <83849198+vanquang202@users.noreply.github.com>
2022-09-21 14:22:43 +0700

    add echo join channel

--- src/app/pages/capacity-play/capacity-play.component.ts
+++ src/app/pages/capacity-play/capacity-play.component.ts
@@ -1,10 +1,10 @@
-import { ActivatedRoute } from "@angular/router";
-import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-capacity-play",
-  templateUrl: "./capacity-play.component.html",
-  styleUrls: ["./capacity-play.component.css"],
+  selector: 'app-capacity-play',
+  templateUrl: './capacity-play.component.html',
+  styleUrls: ['./capacity-play.component.css'],
 })
 export class CapacityPlayComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
@@ -12,7 +12,21 @@ export class CapacityPlayComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       const { code } = params;
-      console.log("Params", code);
+      console.log('Params', code);
+
+      (window as any).Echo.join('room.' + code)
+        .here((users: any) => {
+          console.log('User online ', users); // Chạy lần đầu tiên
+        })
+        .joining((user: any) => {
+          console.log('User joining', user); // User  khác tham gia sẽ chạy hàm này
+        })
+        .leaving((user: any) => {
+          console.log('User leave ', user); //  User thoát phòng
+        })
+        .listen('PersenChannel', function (data: any) {
+          console.log(data); // Lắng nghe sự kiện
+        });
     });
   }
 }

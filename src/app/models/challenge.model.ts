300d7449c796a50c1237ba3428761629eae31534
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-10-14 13:18:20 +0700

    refactor: update ui challenge page

--- src/app/models/challenge.model.ts
+++ src/app/models/challenge.model.ts
@@ -5,4 +5,15 @@ export class Challenge {
   type: number;
   status: number;
   successRef: number;
+  sample_code: {
+    code_language: CodeLang;
+  }[];
+  codeLangs: CodeLang[];
+  tooltipLang: string;
+}
+
+export class CodeLang {
+  id: number;
+  ex: string;
+  type: string;
 }
--- src/app/pages/challenge/challenge.component.ts
+++ src/app/pages/challenge/challenge.component.ts
@@ -9,6 +9,7 @@ import { ChallengeService } from "src/app/services/challenge.service";
 })
 export class ChallengeComponent implements OnInit {
   challenges!: Challenge[];
+  limitLangs = 4;
 
   constructor(private challengeService: ChallengeService) {}
 
@@ -19,9 +20,40 @@ export class ChallengeComponent implements OnInit {
   getChallenges() {
     this.challengeService.getChallenges().subscribe((res) => {
       if (res.status) {
-        this.challenges = res.payload;
-        console.log(this.challenges);
+        this.challenges = res.payload.map((item: Challenge) => {
+          // ds ngôn ngữ code
+          const codeLangs = item.sample_code.map((sampleItem) => sampleItem.code_language);
+
+          // tooltip khi vượt quá limit
+          let tooltipLang = "";
+          if (codeLangs.length > this.limitLangs) {
+            tooltipLang = codeLangs
+              .slice(this.limitLangs)
+              .map((x) => x.ex.toUpperCase())
+              .join(", ");
+          }
+
+          return {
+            ...item,
+            tooltipLang,
+            codeLangs,
+          };
+        });
       }
     });
+  }
+
+  // get mức độ thử thách
+  getTypeChallenge(type: number): string {
+    switch (type) {
+      case 0:
+        return "Mức độ dễ";
+      case 1:
+        return "Mức độ trung bình";
+      case 2:
+        return "Mức độ khó";
+      default:
+        return "Lỗi";
+    }
   }
 }

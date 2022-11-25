601a1cac3f3534747c245df50d03fecbf7ee9267
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-11-25 13:43:42 +0700

    refactor: update save current route (join-capacity page)

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { CanActivate, Router, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { UserService } from "../services/user.service";
 
@@ -9,10 +9,7 @@ import { UserService } from "../services/user.service";
 export class AuthGuard implements CanActivate {
   constructor(private router: Router, private userService: UserService) {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot,
-  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     const user = this.userService.getUserValue();
     const jwtToken = this.userService.getJwtToken();
     const isLogged = user && jwtToken;

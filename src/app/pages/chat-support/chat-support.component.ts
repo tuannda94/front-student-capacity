500f90220c085ec31a5cc9cfe2511fb0a59cdb91
vanquang202 <83849198+vanquang202@users.noreply.github.com>
2022-12-12 15:10:47 +0700

    add support chat

--- src/app/pages/chat-support/chat-support.component.ts
+++ src/app/pages/chat-support/chat-support.component.ts
@@ -1,16 +1,132 @@
 import { Component, OnInit } from "@angular/core";
 import { Title } from "@angular/platform-browser";
 import { ChatSupportService } from "src/app/services/chat-support.service";
-
+import { UserService } from "src/app/services/user.service";
 @Component({
   selector: "app-chat-support",
   templateUrl: "./chat-support.component.html",
   styleUrls: ["./chat-support.component.css"],
 })
 export class ChatSupportComponent implements OnInit {
-  constructor(private titleService: Title, private chatSPService: ChatSupportService) {}
+  constructor(
+    private titleService: Title,
+    private userService: UserService,
+    private chatSPService: ChatSupportService,
+  ) {}
+  users: any = [];
+  data_chat: any = [];
+  authId = 0;
+  authFr = 0;
+  dataChatValue = "";
+  roomCode = "";
+  chatActive: any = [];
 
   ngOnInit(): void {
+    const user = this.userService.getUserValue();
+    this.authId = user.id;
+
     this.titleService.setTitle("Hỗ trợ trực tuyến");
+    this.chanel();
+  }
+
+  sendDataChat() {
+    var that = this;
+    this.chatSPService
+      .sendDataChat({
+        room: this.roomCode,
+        message: this.dataChatValue,
+      })
+      .subscribe(
+        (res: any) => {},
+        (err: any) => {},
+      );
+    this.dataChatValue = "";
+  }
+
+  showChat(authFr: any) {
+    this.roomCode = this.authId + "-" + authFr;
+    this.renderUser();
+  }
+  renderUser() {
+    var that = this;
+    this.chatActive = this.data_chat.filter(function (data: any) {
+      return data.id == that.roomCode;
+    });
+  }
+  checkUserGetKey(id: any) {
+    var key = null;
+    this.data_chat.filter(function (data: any, k: any) {
+      if (data.id == id) key = k;
+    });
+    return key;
+  }
+  privateChannel(code: any = null) {
+    var that = this;
+    (window as any).Echo.private(`support.poly.${code ?? this.roomCode}`).listen("ChatSupportEvent", (e: any) => {
+      var k: any = that.checkUserGetKey(e.data.room);
+      that.data_chat[k].data.push(e.data);
+
+      var dataFist = that.data_chat[0];
+      that.data_chat[0] = that.data_chat[k];
+      that.data_chat[k] = dataFist;
+    });
+  }
+  chanel() {
+    var that = this;
+    (window as any).Echo.join("support.poly")
+      .here((users: any) => {
+        that.users = users;
+        // console.log(users);
+
+        that.users.map(function (user: any) {
+          const checkHasExits = that.data_chat.filter(function (data: any) {
+            return data.user.id == user.id;
+          });
+          if (checkHasExits.length == 0 && user.ctv_st_p == true) {
+            that.privateChannel(that.authId + "-" + user.id);
+            that.data_chat.push({
+              id: that.authId + "-" + user.id,
+              data: [],
+              user: user,
+            });
+          }
+        });
+        $("#kt_aside_toggle").click();
+      })
+      .joining((user: any) => {
+        that.users.push(user);
+        const checkHasExits = that.data_chat.filter(function (data: any) {
+          return data.user.id == user.id;
+        });
+        if (checkHasExits.length == 0 && user.ctv_st_p == true) {
+          that.privateChannel(that.authId + "-" + user.id);
+          that.data_chat.push({
+            id: that.authId + "-" + user.id,
+            data: [],
+            user: user,
+          });
+        }
+      })
+      .leaving((user: any) => {
+        var us = that.users.filter(function (data: any) {
+          return data.id !== user.id;
+        });
+
+        if (user.ctv_st_p == true) {
+          that.data_chat = that.data_chat.filter(function (data: any) {
+            return data.user.id != user.id;
+          });
+        }
+        that.chatActive = [];
+        (window as any).Echo.leave(`private-support.poly.${that.authId + "-" + user.id}`);
+        that.users = us;
+      });
+  }
+
+  ngOnDestroy(): void {
+    console.log("ThoaT");
+
+    (window as any).Echo.leave(this.roomCode);
+    (window as any).Echo.leave("support.poly");
   }
 }
--- src/app/services/chat-support.service.ts
+++ src/app/services/chat-support.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { environment } from "src/environments/environment";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ChatSupportService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  sendDataChat(data: any): any {
+    return this.http.post<any>(`${environment.v1ApiUrl}/fake-post`, data);
+  }
 }

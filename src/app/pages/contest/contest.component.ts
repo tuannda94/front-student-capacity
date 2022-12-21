2ff56b8f5865d04ac2a31723ef5a48cee31e5b02
Uy nguyễn <88134149+uynnph15055@users.noreply.github.com>
2022-12-22 03:07:42 +0700

    Fix bug dự án

--- src/app/pages/contest/contest.component.ts
+++ src/app/pages/contest/contest.component.ts
@@ -1,19 +1,19 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { Title } from '@angular/platform-browser';
-import { ActivatedRoute, Router } from '@angular/router';
-import * as moment from 'moment';
-import { Contest } from 'src/app/models/contest';
-import { Major } from 'src/app/models/major';
-import { ConfigFunctionService } from 'src/app/services/config-function.service';
-import { ContestService } from 'src/app/services/contest.service';
-import { MajorService } from 'src/app/services/major.service';
-import { UserService } from 'src/app/services/user.service';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+import { Title } from "@angular/platform-browser";
+import { ActivatedRoute, Router } from "@angular/router";
+import * as moment from "moment";
+import { Contest } from "src/app/models/contest";
+import { Major } from "src/app/models/major";
+import { ConfigFunctionService } from "src/app/services/config-function.service";
+import { ContestService } from "src/app/services/contest.service";
+import { MajorService } from "src/app/services/major.service";
+import { UserService } from "src/app/services/user.service";
 
 @Component({
-  selector: 'app-contest',
-  templateUrl: './contest.component.html',
-  styleUrls: ['./contest.component.css'],
+  selector: "app-contest",
+  templateUrl: "./contest.component.html",
+  styleUrls: ["./contest.component.css"],
 })
 export class ContestComponent implements OnInit {
   majors: Array<Major>;
@@ -34,24 +34,22 @@ export class ContestComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private titleService: Title,
-    private configFuntionService: ConfigFunctionService
+    private configFuntionService: ConfigFunctionService,
   ) {}
 
   formSearchMajor = new FormGroup({
-    keywordMajor: new FormControl(''),
+    keywordMajor: new FormControl(""),
   });
 
   formSearchContest = new FormGroup({
-    keywordContest: new FormControl(''),
+    keywordContest: new FormControl(""),
   });
 
   ngOnInit(): void {
     // Load lên đầu trang
     this.configFuntionService.runTop();
 
-    this.userService.getUserValue().id != undefined
-      ? (this.checkUserHasLogin = true)
-      : this.checkUserHasLogin;
+    this.userService.getUserValue().id != undefined ? (this.checkUserHasLogin = true) : this.checkUserHasLogin;
 
     this.route.queryParamMap.subscribe((params) => {
       this.orderObj = { ...params };
@@ -60,55 +58,48 @@ export class ContestComponent implements OnInit {
     if (this.orderObj.params.status || this.orderObj.params.keyword) {
       this.statusCurrContest = this.orderObj.params.status;
       this.keyworkSearchContest = this.orderObj.params.keyword;
-      this.formSearchContest.controls['keywordContest'].setValue(
-        this.keyworkSearchContest
-      );
+      this.formSearchContest.controls["keywordContest"].setValue(this.keyworkSearchContest);
     }
 
     if (this.orderObj.params.major_id) {
-      this.majorService
-        .getMajorWhereSlug(this.orderObj.params.major)
-        .subscribe((res) => {
-          this.major_id = res.payload.id;
-        });
+      this.majorService.getMajorWhereSlug(this.orderObj.params.major).subscribe((res) => {
+        this.major_id = res.payload.id;
+      });
     }
 
     this.filterContest();
 
-    this.titleService.setTitle('Cuộc thi');
+    this.titleService.setTitle("Cuộc thi");
 
-    window.addEventListener('scroll', this.scrollNavSub);
+    window.addEventListener("scroll", this.scrollNavSub);
 
     this.getAllMajor();
   }
 
   scrollNavSub() {
-    let element = document.querySelector('.contest__nav');
+    let element = document.querySelector(".contest__nav");
     if (window.scrollY > 250) {
-      element?.classList.add('fixed-nav');
+      element?.classList.add("fixed-nav");
     } else {
-      element?.classList.remove('fixed-nav');
+      element?.classList.remove("fixed-nav");
     }
   }
 
   // Tìm kiếm chuyên ngành
   searchMajor() {
     this.statusMajor = false;
-    this.majorService
-      .searchMajor(this.formSearchMajor.controls['keywordMajor'].value)
-      .subscribe((res) => {
-        if (res.status) {
-          this.majors = res.payload;
-          this.statusMajor = true;
-        }
-      });
+    this.majorService.searchMajor(this.formSearchMajor.controls["keywordMajor"].value).subscribe((res) => {
+      if (res.status) {
+        this.majors = res.payload;
+        this.statusMajor = true;
+      }
+    });
   }
 
   // Tìm kiếm cuộc thi
   searchContest() {
     this.statusContest = false;
-    this.keyworkSearchContest =
-      this.formSearchContest.controls['keywordContest'].value;
+    this.keyworkSearchContest = this.formSearchContest.controls["keywordContest"].value;
     this.filterContest();
   }
 
@@ -126,7 +117,7 @@ export class ContestComponent implements OnInit {
   // Reset Chuyên ngành
   resetMajor() {
     this.major_id = null;
-    this.formSearchMajor.controls['keywordMajor'].setValue(null);
+    this.formSearchMajor.controls["keywordMajor"].setValue(null);
     this.major_slug = null;
     this.filterContest();
     this.getAllMajor();
@@ -136,32 +127,28 @@ export class ContestComponent implements OnInit {
   updateStatusContest(event: any, status: number) {
     if (this.statusContest) {
       this.statusCurrContest = status;
-      const statusAll = document.querySelectorAll('.contest__nav-item');
+      const statusAll = document.querySelectorAll(".contest__nav-item");
       for (let i = 0; i < statusAll.length; i++) {
-        statusAll[i]?.classList.remove('active');
+        statusAll[i]?.classList.remove("active");
       }
       this.filterContest();
-      event.currentTarget.classList.add('active');
+      event.currentTarget.classList.add("active");
     }
   }
 
   // Function dùng chung để lọc sản phẩm
   filterContest() {
     this.statusContest = false;
-    this.router.navigate(['/cuoc-thi'], {
+    this.router.navigate(["/cuoc-thi"], {
       queryParams: {
         status: this.statusCurrContest,
         keyword: this.keyworkSearchContest,
         major: this.major_slug,
       },
-      queryParamsHandling: 'merge',
+      queryParamsHandling: "merge",
     });
     this.contestService
-      .filterContest(
-        this.keyworkSearchContest,
-        this.major_id,
-        this.statusCurrContest
-      )
+      .filterContest(this.keyworkSearchContest, this.major_id, this.statusCurrContest)
       .subscribe((res) => {
         if (res.status) {
           this.statusContest = true;
@@ -176,24 +163,19 @@ export class ContestComponent implements OnInit {
           } else if (this.statusCurrContest == 0) {
             this.contests = contests.filter((item: Contest) => {
               let time = this.getTime(item);
-              return (
-                time.date_end > today && !(time.date_register_start > today)
-              );
+              return time.date_end > today && !(time.date_register_start > today);
             });
           } else {
             this.contests = contests;
           }
         }
       });
+    this.configFuntionService.runTop();
   }
 
   getTime(item: Contest) {
-    let date_end = new Date(
-      moment(item.register_deadline).format('lll')
-    ).getTime();
-    let date_register_start = new Date(
-      moment(item.start_register_time).format('lll')
-    ).getTime();
+    let date_end = new Date(moment(item.register_deadline).format("lll")).getTime();
+    let date_register_start = new Date(moment(item.start_register_time).format("lll")).getTime();
 
     return {
       date_end: date_end,
@@ -203,16 +185,14 @@ export class ContestComponent implements OnInit {
 
   // Gọi các cuộc thi theo chuyên ngành
   getWhereMajor(event: any, item: Major) {
-    const majors = document.querySelectorAll(
-      '.contest__content-aside-major--item'
-    );
+    const majors = document.querySelectorAll(".contest__content-aside-major--item");
     majors.forEach((element) => {
-      element.classList.remove('active');
+      element.classList.remove("active");
     });
 
     this.major_slug = item.slug;
     this.major_id = item.id;
-    event.currentTarget.classList.add('active');
+    event.currentTarget.classList.add("active");
     this.filterContest();
   }
 }
